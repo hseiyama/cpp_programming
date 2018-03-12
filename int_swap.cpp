1bac3203e29bfa861a8e8c120d3bdca3507ba128
Hirofumi Seiyama <hseiyama@gmail.com>
2018-03-12 23:44:13 +0900

    add file (2)

---
+++ int_swap.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+template <class Type> 
+void myswap( Type &a, Type &b )
+{
+	cout << "Type version is called" << endl;
+
+	Type temp = a;
+
+	a = b;
+	b = temp;
+}
+
+template<>
+void myswap<int>( int &a, int &b )
+{
+	cout << "int version is called" << endl;
+
+	int temp = a;
+
+	a = b;
+	b = temp;
+}
+
+int main()
+{
+	int    a = 1,   b = 2;
+	double x = 1.0, y = 2.0;
+
+	cout << "a=" << a << ",b=" << b << endl;
+	myswap( a, b );
+	cout << "a=" << a << ",b=" << b << endl;
+
+	cout << "x=" << x << ",y=" << y << endl;
+	myswap( x, y );
+	cout << "x=" << x << ",y=" << y << endl;
+
+	return 0;
+}
