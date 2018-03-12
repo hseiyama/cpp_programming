236d5faae879eef511cd3db366204b75c3c6556a
Hirofumi Seiyama <hseiyama@gmail.com>
2018-03-12 23:55:35 +0900

    add file (3)

---
+++ temp_swap_class.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+template <class Type> 
+void myswap( Type &a, Type &b )
+{
+	Type temp = a;
+
+	a = b;
+	b = temp;
+}
+
+int main()
+{
+	string s1 = "one";
+	string s2 = "two";
+
+	cout << "s1=" << s1 << ",s2=" << s2 << endl;
+	myswap( s1, s2 );
+	cout << "s1=" << s1 << ",s2=" << s2 << endl;
+
+	return 0;
+}
